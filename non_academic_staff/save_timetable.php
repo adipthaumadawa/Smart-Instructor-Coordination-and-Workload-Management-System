f4c8eb9e2aa23669cc07b9deea37a75d16d3b0fc
Rizan-6 <mohamedrizan315@gmail.com>
2026-07-31 22:22:19 +0530

    UPDATE CODE

--- non_academic_staff/save_timetable.php
+++ non_academic_staff/save_timetable.php
@@ -5,64 +5,66 @@ session_start();
 include("../config/db.php");
 
 
-// Check form submit
-
 if(isset($_POST['subject_name'])){
 
 
-    // Get values from form
-
     $subject_name = $_POST['subject_name'];
-
     $course = $_POST['course'];
-
     $day_name = $_POST['day_name'];
-
     $start_time = $_POST['start_time'];
-
     $end_time = $_POST['end_time'];
-
     $room = $_POST['room'];
-
     $semester = $_POST['semester'];
-
     $academic_year = $_POST['academic_year'];
 
 
 
-    // Insert Query
-
-    $sql = "INSERT INTO timetables
-    (
-        subject_name,
-        course,
-        day_name,
-        start_time,
-        end_time,
-        room,
-        semester,
-        academic_year
-    )
-
-    VALUES
-
-    (
-        '$subject_name',
-        '$course',
-        '$day_name',
-        '$start_time',
-        '$end_time',
-        '$room',
-        '$semester',
-        '$academic_year'
-    )";
+    try {
 
 
+        $sql = "INSERT INTO timetables
+        (
+            subject_name,
+            course,
+            day_name,
+            start_time,
+            end_time,
+            room,
+            semester,
+            academic_year
+        )
 
-    // Execute Query
+        VALUES
 
-    if(mysqli_query($conn,$sql))
-    {
+        (
+            :subject_name,
+            :course,
+            :day_name,
+            :start_time,
+            :end_time,
+            :room,
+            :semester,
+            :academic_year
+        )";
+
+
+        $stmt = $pdo->prepare($sql);
+
+
+        $stmt->execute([
+
+            ':subject_name'=>$subject_name,
+            ':course'=>$course,
+            ':day_name'=>$day_name,
+            ':start_time'=>$start_time,
+            ':end_time'=>$end_time,
+            ':room'=>$room,
+            ':semester'=>$semester,
+            ':academic_year'=>$academic_year
+
+        ]);
+
+
 
         echo "
         <script>
@@ -71,23 +73,21 @@ if(isset($_POST['subject_name'])){
         </script>
         ";
 
-    }
-    else
-    {
 
-        echo "Database Error : ".mysqli_error($conn);
+
+    } catch(PDOException $e){
+
+        echo "Error: ".$e->getMessage();
 
     }
 
 
 }
 
-else
-{
+else{
 
-    echo "No Data Received";
+    echo "No data received";
 
 }
-
 
 ?>
